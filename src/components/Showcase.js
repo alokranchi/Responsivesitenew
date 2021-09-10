f01f44abf54d8dddb408323199225a634a7fdd3e
Alok Mukherjee <in117f@linde.com>
2021-09-10 21:58:39 +0530

    message about the commit

---
+++ src/components/Showcase.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Showcase = () => {
+    return (
+        <div className="show">
+            <div className="container rel">
+           <div className="show__content">
+               <h1>Client experiences</h1>
+               <h1>Client satisfaction.</h1>
+               </div>
+
+               <div className="show__card">
+                   <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
+                   <div className="show__card-h4">What Client's Say?</div>
+                   <div className="show__card-text">According to experience from custormer and there openion makes the difference by enhancement on the webpages for effective</div>
+               </div>
+               </div> 
+        </div>
+    )
+}
+
+export default Showcase
